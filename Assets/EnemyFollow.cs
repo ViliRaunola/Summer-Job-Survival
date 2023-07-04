d94aeea5b3700b8133f9f4e86379a5797587cc7a
Vili Raunola <raunolavili@gmail.com>
2023-07-04 19:18:14 +0300

    Enemies follow player and first map concept

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reference: https://www.youtube.com/watch?v=2SXa10ILJms
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public float enemySpeed;
+    public float followDistance;
+
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        
+
+        if(distance < followDistance) {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+    }
+}
